b283f29828dc00d9779314e0bdae138facae8e0a
Akingboye Marto <martinsakingboye8@gmail.com>
2024-02-26 12:42:34 +0100

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Reaction Time: {{score}} ms
+   <p class="rank"> {{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Ninja Fingers'
+        }else if(this.score < 400){
+            this.rank = 'Rapid Reflexese'
+        }
+        else{
+            this.rank = 'snail pace...'
+        }
+    }
+
+
+}
+</script>
+
+<style>
+.rank{
+    font-size:1.4em;
+    color: #0faf87;
+    font-weight: bold;
+    
+}
+</style>
